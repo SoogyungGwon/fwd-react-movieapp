658488f93debf60add0986da09d6f5ad957b034c
sgwon <soogyung.gwon.ca@gmail.com>
2024-02-19 17:47:17 -0800

    API connect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,46 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Nav from './Nav';
+import { NavLink } from 'react-router-dom';
+import { useState } from 'react'
+import { searchBaseURL, apiKey } from '../globals/globalVariables';
 
-const Header = () => {
+const Header = ( { url_set, setUrl } ) => {
+
+    const [search, setSearch] = useState("");
+    
+    const searchMovie = (evt) => {
+        if(evt.key=="Enter"){
+            let url=`${searchBaseURL}`+`${apiKey}`+'&query=' +`${search}`;
+            console.log( {url} );
+            setUrl(url);
+            setSearch("");
+        }
+    }
+
+    const searchEntered = (e) => {
+
+        e.preventDefault();
+        setSearch(e.target.value);
+    }
+
     return (
         <header>
-            <Nav />
+        <nav>
+            <ul className="main-nav">
+                <li><NavLink to="/">Moviesite</NavLink></li>
+                <li><NavLink to="/favourite">List</NavLink></li>
+                <li><NavLink to="/about">About</NavLink></li>
+                {/* temporary link below */}
+                <li><NavLink to="/movie">Temporary Movie</NavLink></li>
+                <li>
+                    {/* <form> */}
+                        <input type="text" placeholder="Search" 
+                        value={search} 
+                        onChange={searchEntered} 
+                        onKeyDown={searchMovie} />
+                        <button name="search-button" onClick={searchMovie}>S</button>
+                    {/* </form> */}
+                </li>
+            </ul>
+        </nav>
         </header>
     );
 }
