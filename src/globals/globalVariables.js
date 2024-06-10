b778536e0bd7bdf53d0e5bc06904c3ad29332174
SoogyungGwon <soogyung.gwon.ca@gmail.com>
2024-06-10 12:03:26 -0700

    revert

--- src/globals/globalVariables.js
+++ src/globals/globalVariables.js
@@ -1,33 +1,5 @@
-//Publish
-const APP_FOLDER_NAME = "moviesite";
-const OTHER_GLOBAL = "other-global";
-export { APP_FOLDER_NAME, OTHER_GLOBAL }
-
-//App info
 export const appTitle = 'Moviesite';
 export const appAuthor = 'Soogyung Gwon';
-export const appStorageName = 'moo-list';
-export const imageFolderPath = import.meta.env.BASE_URL + 'assets/';
-
-//api
-export const apiKey = "&api_key=f8bcc12bd6f6afe7a33a79281ec21649";
-
-//Genre
-export const popularQuery = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
-export const topRatedQuery = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
-
-export const nowPlayingQuery = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
-export const upcomingQuery = 'https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1';
+export const apiKey = "f8bcc12bd6f6afe7a33a79281ec21649";
 
 
-//image
-export const imageBaseURL = 'https://image.tmdb.org/t/p/w500/';
-
-//Search
-export const searchBaseURL = 'https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&query=';
-
-//Single movie ID query
-export const singleMoveQuery = 'https://api.themoviedb.org/3/movie/';
-
-//Finding Actor
-//https://api.themoviedb.org/3/search/keyword?query=%27harry%27&page=1&api_key=f8bcc12bd6f6afe7a33a79281ec21649
